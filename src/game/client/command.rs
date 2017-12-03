9e8040f2f41ab661d912dd29deec416a83b4bb41
rightfold <rightfold@gmail.com>
2017-12-03 21:05:31 +0100

    Accept short commands and trim commands

--- src/game/client/command.rs
+++ src/game/client/command.rs
@@ -2,12 +2,12 @@ use infra::common::direction::Direction;
 use infra::common::protocol::Request;
 
 pub fn parse_command(command: &str) -> Option<Request> {
-    match command {
-        "look"  => Some(Request::Look),
-        "north" => Some(Request::Move(Direction::North)),
-        "south" => Some(Request::Move(Direction::South)),
-        "west"  => Some(Request::Move(Direction::West )),
-        "east"  => Some(Request::Move(Direction::East )),
+    match command.trim() {
+        "l" | "look"  => Some(Request::Look),
+        "n" | "north" => Some(Request::Move(Direction::North)),
+        "s" | "south" => Some(Request::Move(Direction::South)),
+        "w" | "west"  => Some(Request::Move(Direction::West )),
+        "e" | "east"  => Some(Request::Move(Direction::East )),
         _ => None,
     }
 }
